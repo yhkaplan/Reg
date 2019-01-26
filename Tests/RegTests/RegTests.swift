2d3600c8203fe19e9d9580bfd5a1466276e0e76b
Joshua Kaplan <yhkaplan@gmail.com>
2019-01-26 15:46:33 +0900

    Add test_hasMatch_returnsFalseWhenNoMatches

--- Tests/RegTests/RegTests.swift
+++ Tests/RegTests/RegTests.swift
@@ -6,6 +6,7 @@ final class RegTests: XCTestCase {
     static var allTests = [
         ("test_matches_returnsEmptyArrayWhenNoMatches", test_matches_returnsEmptyArrayWhenNoMatches),
         ("test_matches_returnsEachMatch", test_matches_returnsEachMatch),
+        ("test_hasMatch_returnsFalseWhenNoMatches", test_hasMatch_returnsFalseWhenNoMatches),
     ]
 
     private let regexes: [Regex] = [
@@ -15,14 +16,28 @@ final class RegTests: XCTestCase {
         "👪+"
     ]
 
-    func test_matches_returnsEmptyArrayWhenNoMatches() {
-        let nonMatchingStrings: [String] = [
-            "Oh hey",
-            "No oh",
-            "%*(())",
-            "🙍"
-        ]
+    private let nonMatchingStrings: [String] = [
+        "Oh hey",
+        "No oh",
+        "%*(())",
+        "🙍"
+    ]
 
+    private let matchingStrings: [String] = [
+        "hey man",
+        "No oh Oh no",
+        "%*(4()45)203-",
+        "👪👪👪🙍👪👪👪👪"
+    ]
+
+    private let expectedResults: [[String]] = [
+        ["hey"],
+        ["Oh no"],
+        ["4", "45", "203"],
+        ["👪👪👪", "👪👪👪👪"]
+    ]
+
+    func test_matches_returnsEmptyArrayWhenNoMatches() {
         regexes.enumerated().forEach { idx, regex in
             let str = nonMatchingStrings[idx]
 
@@ -33,19 +48,6 @@ final class RegTests: XCTestCase {
     }
 
     func test_matches_returnsEachMatch() {
-        let matchingStrings: [String] = [
-            "hey man",
-            "No oh Oh no",
-            "%*(4()45)203-",
-            "👪👪👪🙍👪👪👪👪"
-        ]
-        let expectedResults: [[String]] = [
-            ["hey"],
-            ["Oh no"],
-            ["4", "45", "203"],
-            ["👪👪👪", "👪👪👪👪"]
-        ]
-
         regexes.enumerated().forEach { idx, regex in
             let str = matchingStrings[idx]
 
@@ -53,6 +55,16 @@ final class RegTests: XCTestCase {
 
             let expected = expectedResults[idx]
             XCTAssertEqual(result, expected)
+        }
+    }
+
+    func test_hasMatch_returnsFalseWhenNoMatches() {
+        regexes.enumerated().forEach { idx, regex in
+            let str = nonMatchingStrings[idx]
+
+            let result = regex.hasMatch(in: str)
+
+            XCTAssertFalse(result)
         }
     }
 
